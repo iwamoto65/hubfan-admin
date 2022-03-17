0293750f130cb51e297e0d317e143a8373550016
iwamoto65 <kosuke00940@icloud.com>
2022-03-17 23:22:54 +0900

    htmlのattr名を変更した

--- app/src/components/atoms/icons/PlayerIcon.tsx
+++ app/src/components/atoms/icons/PlayerIcon.tsx
@@ -1,7 +1,7 @@
 export const PlayerIcon = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+      <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
     </svg>
   )
 }
