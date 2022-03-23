517f09afc467022485c19674d773485455749b4d
iwamoto65 <kosuke00940@icloud.com>
2022-03-24 00:20:30 +0900

    検索機能を追加した

--- app/src/pages/teams/index.tsx
+++ app/src/pages/teams/index.tsx
@@ -1,27 +1,38 @@
 import type { NextPage } from 'next';
-import useSWR, { SWRConfig } from 'swr';
+import useSWR from 'swr';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { fetchData } from 'src/utils/api/fetchData';
 import { GreenEditButton } from 'src/components/atoms/buttons/GreenEditButton';
 import { RedDeleteButton } from 'src/components/atoms/buttons/RedDeleteButton';
 
-export const getStaticProps = async () => {
-  const res = await fetchData('/v1/teams');
-
-  return {
-    props: {
-      fallback: {
-        '/teams': res
-      }
-    }
-  }
+type Inputs = {
+  name: string
 }
 
-const TeamContainer: NextPage = () => {
-  const { data } = useSWR('/teams')
-  const teams:  { id: number, name: string, dateEstablished: string }[] = data;
+const Team: NextPage = () => {
+  const [nameSearchWord, setNameSearchWord] = useState('');
+
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => setNameSearchWord(data.name);
+
+  const fetcher = async (path: string) => await fetchData(path);
+  const { data: teams } = useSWR('/v1/teams' + `?q[name_cont]=${nameSearchWord}`, fetcher);
+  const hitCount: number = teams ? teams.length : 0;
 
   return (
     <>
+      <form onSubmit={handleSubmit(onSubmit)} className="m-10" >
+        <label htmlFor="name">チーム名</label>
+        <br />
+        <input type="text" defaultValue={nameSearchWord} {...register("name")} />
+        <br />
+        {errors.name && <span>エラー：{ errors }</span>}
+        <button type='submit'>検索</button>
+      </form>
+
+      <div>{ hitCount }件</div>
+
       <div className='flex justify-center'>
         <table className="w-11/12 mt-10">
           <thead>
@@ -33,7 +44,7 @@ const TeamContainer: NextPage = () => {
             </tr>
           </thead>
           <tbody>
-            {teams.map(team => (
+            {teams?.map(team => (
               <tr className='text-left border-b border-gray-300' key={team.id}>
                 <td className="px-3 py-3">{ team.name }</td>
                 <td className="px-3 py-3">{ team.dateEstablished }</td>
@@ -45,14 +56,6 @@ const TeamContainer: NextPage = () => {
         </table>
       </div>
     </>
-  )
-}
-
-const Team = ({ fallback }: { [key: string]: any }) => {
-  return (
-    <SWRConfig value={{ fallback }}>
-      <TeamContainer />
-    </SWRConfig>
   )
 }
 
--- app/src/utils/api/fetchData.ts
+++ app/src/utils/api/fetchData.ts
@@ -1,7 +1,7 @@
 import camelcaseKeys from "camelcase-keys"
 
-export const fetchData = async (path: string): Promise<{ [key: string]: any }[]> => {
-  const endpoint = 'http://hubfan_rails_backend:3000/api/admin' + path;
+export const fetchData = async (path: string): Promise<{ [key: string]: string | number | null }[]> => {
+  const endpoint = 'http://localhost:3000/api/admin' + path;
   const res = await fetch(endpoint, { method: 'GET' }).then(res => res.json());
 
   return camelcaseKeys(res);
